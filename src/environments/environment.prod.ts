b2f0ca514152808e0ca782b9557aef883e13515f
QuangNV <quangnv1311@gmail.com>
2021-07-15 11:43:24 +0700

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   appVersion: 'v726demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: false,
-  apiUrl: 'http://sunoil-driver.firecloud.live/'
+  apiUrl: 'https://sunoil-driver.firecloud.live/drivers'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   appVersion: 'v726demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: false,
-  apiUrl: 'http://sunoil-driver.firecloud.live'
+  apiUrl: 'https://sunoil-driver.firecloud.live/drivers'
 };
 
 /*
