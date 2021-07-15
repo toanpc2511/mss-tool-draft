8990a2f1f456d3e0e2c33c10f449606604ba48ba
QuangNV <quangnv1311@gmail.com>
2021-07-15 11:43:37 +0700

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,5 @@ export const environment = {
   appVersion: 'v726demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: false,
-  apiUrl: 'http://sunoil-management.firecloud.live'
-  // apiUrl: 'mysite.com/api'
+  apiUrl: 'https://sunoil-management.firecloud.live'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   appVersion: 'v726demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: false,
-  apiUrl: 'http://sunoil-management.firecloud.live'
+  apiUrl: 'https://sunoil-management.firecloud.live'
 };
 
 /*
