ceb7c156c80fbea4b568a3a70403b4e3d237a6df
QuangNV <quangnv1311@gmail.com>
2021-07-15 13:09:45 +0700

    hot fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   appVersion: 'v726demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: false,
-  apiUrl: 'https://sunoil-management.firecloud.live'
+  apiUrl: 'https://sunoil-management.firecloud.live/management'
 };
