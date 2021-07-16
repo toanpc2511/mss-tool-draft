cbce29aa3ac99043bd0fb6e8ff5c56d2563d09f2
QuangNV <quangnv1311@gmail.com>
2021-07-16 14:30:02 +0700

    fix

--- src/app/modules/gas-station/create-station/create-station.component.ts
+++ src/app/modules/gas-station/create-station/create-station.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-create-station',
   templateUrl: './create-station.component.html',
   styleUrls: ['./create-station.component.scss']
 })
-export class CreateStationComponent implements OnInit {
+export class CreateStationComponent implements OnInit, AfterViewInit, OnDestroy {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
   }
 
+  ngOnDestroy() {
+  }
 }
