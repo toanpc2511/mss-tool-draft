99990f69c257b8647391ca87e6c7a05ad2bdfa5e
Bui Thanh Tuan <tuanbt209@gmail.com>
2021-07-16 09:52:16 +0700

    list gas station, create station

--- src/app/pages/gas-station/list-station/list-station.component.ts
+++ src/app/pages/gas-station/list-station/list-station.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CreateStationModalComponent } from '../create-station-modal/create-station-modal.component';
 
+export interface PeriodicElement {
+  code: string;
+  name: string;
+  location: string;
+  status: boolean;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    code: 'ST01',
+    name: 'Sun Oil 01',
+    location: 'Tầng 20 Charmvit Tower, 117 Trần Duy Hưng, Trung Hòa, Cầu Giấy, Hà Nội',
+    status: true
+  },
+  {
+    code: 'ST02',
+    name: 'Sun Oil 02',
+    location: 'Tầng 20 Charmvit Tower, 117 Trần Duy Hưng, Trung Hòa, Cầu Giấy, Hà Nội',
+    status: true
+  },
+  {
+    code: 'ST03',
+    name: 'Sun Oil 03',
+    location: 'Tầng 20 Charmvit Tower, 117 Trần Duy Hưng, Trung Hòa, Cầu Giấy, Hà Nội',
+    status: false
+  },
+  {
+    code: 'ST04',
+    name: 'Sun Oil 04',
+    location: 'Tầng 20 Charmvit Tower, 117 Trần Duy Hưng, Trung Hòa, Cầu Giấy, Hà Nội',
+    status: true
+  },
+  {
+    code: 'ST05',
+    name: 'Sun Oil 05',
+    location: 'Tầng 20 Charmvit Tower, 117 Trần Duy Hưng, Trung Hòa, Cầu Giấy, Hà Nội',
+    status: true
+  }
+];
+
+/**
+ * @title Table with sorting
+ */
 @Component({
   selector: 'app-list-station',
-  templateUrl: './list-station.component.html',
-  styleUrls: ['./list-station.component.scss']
+  styleUrls: ['list-station.component.scss'],
+  templateUrl: 'list-station.component.html'
 })
-export class ListStationComponent implements OnInit {
+export class ListStationComponent {
+  dataSource: PeriodicElement[] = ELEMENT_DATA;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit(): void {
+  createStation() {
+    const modalRef = this.modalService.open(CreateStationModalComponent, { size: 'xl' });
   }
-
 }
