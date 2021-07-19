40fcea1a0b0d0e1aa2f51e6cd8f23be24ddf2f63
QuangNV <quangnv1311@gmail.com>
2021-07-19 08:39:50 +0700

    add service

---
+++ src/app/modules/gas-station/gas-station.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GasStationService {
+  // List gas station
+
+  // Create gas station
+
+  // Step 1
+
+  // Step 2
+
+  // Step 3
+
+  // Step 4
+
+  constructor() {}
+
+  // List gas station
+
+  // Create gas station
+
+  // Step 1
+
+  // Step 2
+
+  // Step 3
+
+  // Step 4
+}
