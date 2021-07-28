63520f6724795027f665392774c8a9a779f97985
ToanPC <toanpc@ttc-solutions.com.vn>
2021-07-28 15:51:44 +0700

    Them chuc năng danh sach nhom san pham

--- src/app/modules/product-type/list-product-type/list-product-type.component.ts
+++ src/app/modules/product-type/list-product-type/list-product-type.component.ts
@@ -1,15 +1,117 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { debounceTime, takeUntil } from 'rxjs/operators';
+import { ConfirmDeleteComponent } from 'src/app/shared/components/confirm-delete/confirm-delete.component';
+import { LIST_STATUS } from 'src/app/shared/data-enum/list-status';
+import { IConfirmModalData } from 'src/app/shared/models/confirm-delete.interface';
+import { DestroyService } from 'src/app/shared/services/destroy.service';
+import { FilterService } from 'src/app/shared/services/filter.service';
+import { SortService } from 'src/app/shared/services/sort.service';
+import { FilterField, SortState } from 'src/app/_metronic/shared/crud-table';
+import { ProductTypeResponse, ProductTypeService } from '../product-type.service';
 
 @Component({
   selector: 'app-list-product-type',
   templateUrl: './list-product-type.component.html',
-  styleUrls: ['./list-product-type.component.scss']
+  styleUrls: ['./list-product-type.component.scss'],
+  providers: [SortService, FilterService, DestroyService]
 })
 export class ListProductTypeComponent implements OnInit {
+  searchFormControl: FormControl;
+  listStatus = LIST_STATUS;
+  dataSource: Array<ProductTypeResponse>;
+  dataSourceTemp: Array<ProductTypeResponse>;
+  sorting: SortState;
 
-  constructor() { }
+  filterField: FilterField<{
+    code: null;
+    name: null;
+    description: null;
+  }>;
 
-  ngOnInit(): void {
+  constructor(
+    private productTypeService: ProductTypeService,
+    private sortService: SortService<ProductTypeResponse>,
+    private filterService: FilterService<ProductTypeResponse>,
+    private cdr: ChangeDetectorRef,
+    private destroy$: DestroyService,
+    private modalService: NgbModal
+  ) {
+    this.dataSource = this.dataSourceTemp = [];
+    this.sorting = sortService.sorting;
+    this.filterField = new FilterField({
+      code: null,
+      name: null,
+      description: null
+    });
+    this.searchFormControl = new FormControl();
+  }
+
+  ngOnInit() {
+    this.getListProductType();
+
+    // Filter
+    this.searchFormControl.valueChanges
+      .pipe(debounceTime(500), takeUntil(this.destroy$))
+      .subscribe((value) => {
+        if (value.trim()) {
+          this.filterField.setFilterFieldValue(value.trim());
+        } else {
+          this.filterField.setFilterFieldValue(null);
+        }
+        // Set data after filter and apply current sorting
+        this.dataSource = this.sortService.sort(
+          this.filterService.filter(this.dataSourceTemp, this.filterField.field)
+        );
+        this.cdr.detectChanges();
+      });
+  }
+
+  // Get list product type
+  getListProductType() {
+    this.productTypeService.getListProductType().subscribe((res) => {
+      this.dataSource = this.dataSourceTemp = res.data;
+      // Set data after filter and apply current sorting
+      this.dataSource = this.sortService.sort(
+        this.filterService.filter(this.dataSourceTemp, this.filterField.field)
+      );
+      this.cdr.detectChanges();
+    });
+  }
+
+  
+  sort(column: string) {
+    this.dataSource = this.sortService.sort(this.dataSource, column);
+  }
+
+  createProductType(): void {
+    alert("Xin chào!")
+  }
+
+  updateProductType(prodTypeId: number): void {
+    alert("Xin chào!")
+  }
+
+  deleteProductType(item: ProductTypeResponse): void {
+    const modalRef = this.modalService.open(ConfirmDeleteComponent, {
+      backdrop: 'static'
+    });
+    const data: IConfirmModalData = {
+      title: 'Xác nhận',
+      message: `Bạn có chắc chắn muốn xoá thông tin nhóm sản phẩm  ${item.code} - ${item.name} ?`,
+      button: { class: 'btn-primary', title: 'Xác nhận' }
+    };
+    modalRef.componentInstance.data = data;
+
+    modalRef.result.then((result) => {
+      if (result) {
+        
+        this.productTypeService.deleteStation(item.id).subscribe(() => {
+          this.getListProductType();
+        });
+      }
+    });
   }
 
 }
