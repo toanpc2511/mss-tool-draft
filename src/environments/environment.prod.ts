4ab9effc894098779ede2890f2d71930460f26e4
QuangNV <quangnv1311@gmail.com>
2021-08-31 13:42:13 +0700

    fix file upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
 	production: true,
 	appVersion: 'v1.0.0',
 	apiUrl: 'https://sunoil-driver.firecloud.live/drivers',
+	apiDriverRoot: 'https://sunoil-driver.firecloud.live',
 	apiManagementUrl: 'https://sunoil-management.firecloud.live/management',
 	apiUrlRoot: 'https://sunoil-management.firecloud.live'
 };
