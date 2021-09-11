7edb68c7bdfd933c5537a151b2a117e1ed3bbe22
ToanPC <toanpc@ttc-solutions.com.vn>
2021-09-11 09:15:07 +0700

    format code

--- src/app/modules/configuration-management/rank-config/rank-config.component.ts
+++ src/app/modules/configuration-management/rank-config/rank-config.component.ts
@@ -7,85 +7,88 @@ import { takeUntil } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-rank-config',
-  templateUrl: './rank-config.component.html',
-  styleUrls: ['./rank-config.component.scss'],
-  providers: [DestroyService, FormBuilder]
+	selector: 'app-rank-config',
+	templateUrl: './rank-config.component.html',
+	styleUrls: ['./rank-config.component.scss'],
+	providers: [DestroyService, FormBuilder]
 })
 export class RankConfigComponent implements OnInit {
-  permissionForm: FormGroup;
-  moduleAccordion: NgbAccordion;
-  groupAccordions: Array<NgbAccordion>;
-  configRankFormArray: FormArray = new FormArray([]);
+	permissionForm: FormGroup;
+	moduleAccordion: NgbAccordion;
+	groupAccordions: Array<NgbAccordion>;
+	configRankFormArray: FormArray = new FormArray([]);
+	arrayLength = [];
 
-  @ViewChild('moduleAccordion', { static: false }) set module(element: NgbAccordion) {
-    this.moduleAccordion = element;
-  }
+	@ViewChild('moduleAccordion', { static: false }) set module(element: NgbAccordion) {
+		this.moduleAccordion = element;
+	}
 
-  constructor(
-    private fb: FormBuilder,
-    private configManagement: ConfigurationManagementService,
-    private cdr: ChangeDetectorRef,
-    private destroy$: DestroyService,
-    private toastr: ToastrService
-  ) {
-  }
+	constructor(
+		private fb: FormBuilder,
+		private configManagement: ConfigurationManagementService,
+		private cdr: ChangeDetectorRef,
+		private destroy$: DestroyService,
+		private toastr: ToastrService
+	) {}
 
-  ngOnInit(): void {
-    this.getListRank();
-  }
+	ngOnInit(): void {
+		this.getListRank();
+	}
 
-  getListRank() {
-    this.configManagement.getListRank()
-      .subscribe((res) => {
-        this.configRankFormArray = this.fb.array(
-          res.data.map((x) => {
-            return this.fb.group({
-              id: [x.id],
-              name: [x.name],
-              score: [x.score],
-              introduction: [x.introduction],
-              policy: [x.policy],
-              promotion: [x.promotion]
-            });
-          })
-        );
-        this.cdr.detectChanges();
-      });
-  }
+	getListRank() {
+		this.configManagement.getListRank().subscribe((res) => {
+			this.configRankFormArray = this.fb.array(
+				res.data.map((x) => {
+					this.arrayLength.push({
+						policy: x.policy.length,
+						introduction: x.introduction.length,
+						promotion: x.promotion.length
+					});
+					return this.fb.group({
+						id: [x.id],
+						name: [x.name],
+						score: [x.score],
+						introduction: [x.introduction],
+						policy: [x.policy],
+						promotion: [x.promotion]
+					});
+				})
+			);
+			this.cdr.detectChanges();
+		});
+	}
 
-  onSubmit() {
-    const req = {
-      rankRequests: this.configRankFormArray.value
-    };
-    this.configManagement.updateRankConfig(req)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(
-        (res) => {
-          if (res.data) {
-            this.toastr.success('Lưu thông tin thành công');
-          }
-        }
-      );
-  }
+	onSubmit() {
+		const req = {
+			rankRequests: this.configRankFormArray.value
+		};
+		this.configManagement
+			.updateRankConfig(req)
+			.pipe(takeUntil(this.destroy$))
+			.subscribe((res) => {
+				if (res.data) {
+					this.toastr.success('Lưu thông tin thành công');
+				}
+			});
+	}
 
-  cancel() {
-    this.getListRank();
-  }
+	cancel() {
+		this.getListRank();
+	}
 
-  isOpenModule(moduleId) {
-    return this.moduleAccordion && this.moduleAccordion.activeIds.includes(moduleId);
-  }
+	isOpenModule(moduleId) {
+		return this.moduleAccordion && this.moduleAccordion.activeIds.includes(moduleId);
+	}
 
-  isOpenGroup(groupId) {
-    return (
-      this.groupAccordions?.length > 0 &&
-      this.groupAccordions.some((ga) => ga.activeIds.includes(groupId))
-    );
-  }
+	isOpenGroup(groupId) {
+		return (
+			this.groupAccordions?.length > 0 &&
+			this.groupAccordions.some((ga) => ga.activeIds.includes(groupId))
+		);
+	}
 
-  autoGrowTextZone(e) {
-    e.target.style.height = "auto";
-    e.target.style.height = (e.target.scrollHeight + 10)+"px";
-  }
+	autoGrowTextZone(e) {
+		e.target.style.height = 'auto';
+		e.target.style.height = e.target.scrollHeight + 10 + 'px';
+	}
 }
