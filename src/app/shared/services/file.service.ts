fe8512632db6d8220f5b25fe90c24a1af5f922e9
QuangNV <quangnv1311@gmail.com>
2021-09-17 16:49:57 +0700

    hot fix

--- src/app/shared/services/file.service.ts
+++ src/app/shared/services/file.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpService } from './http.service';
 import { saveAs } from 'file-saver';
+import { ToastrService } from 'ngx-toastr';
 
 export enum EFileType {
 	IMAGE = 'IMAGE',
@@ -25,7 +26,11 @@ export interface IUploadProgress {
 	providedIn: 'root'
 })
 export class FileService {
-	constructor(private http: HttpService, private httpClient: HttpClient) {}
+	constructor(
+		private http: HttpService,
+		private httpClient: HttpClient,
+		private toastr: ToastrService
+	) {}
 
 	downloadFile(fileId: string, fileName: string) {
 		const params = new HttpParams().set('file-id', fileId);
@@ -35,9 +40,12 @@ export class FileService {
 				observe: 'response',
 				responseType: 'blob'
 			})
-			.subscribe((res) => {
-				saveAs(res.body, fileName);
-			});
+			.subscribe(
+				(res) => {
+					saveAs(res.body, fileName);
+				},
+				() => this.toastr.error('Bạn không có quyền tải file này')
+			);
 	}
 
 	uploadFile(fileFormData: FormData, type: EFileType) {
