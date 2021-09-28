bf3d9a1d302059487cc9402ac11b2e4dadab4e10
ToanPC <toanpc@ttc-solutions.com.vn>
2021-09-28 09:35:11 +0700

    hot fix

--- src/app/modules/configuration-management/promotion-config/promotion-config.component.ts
+++ src/app/modules/configuration-management/promotion-config/promotion-config.component.ts
@@ -9,125 +9,128 @@ import { IConfirmModalData } from '../../../shared/models/confirm-delete.interfa
 import { IError } from '../../../shared/models/error.model';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
-import { ConfigurationManagementService, IConfigPromotion } from '../configuration-management.service';
+import {
+	ConfigurationManagementService,
+	IConfigPromotion
+} from '../configuration-management.service';
 import { debounceTime, takeUntil } from 'rxjs/operators';
-import { IDataTransfer, PromotionConfigModalComponent } from '../promotion-config-modal/promotion-config-modal.component';
+import {
+	IDataTransfer,
+	PromotionConfigModalComponent
+} from '../promotion-config-modal/promotion-config-modal.component';
 
 @Component({
-  selector: 'app-promotion-config',
-  templateUrl: './promotion-config.component.html',
-  styleUrls: ['./promotion-config.component.scss'],
-  providers: [SortService, FilterService, DestroyService]
+	selector: 'app-promotion-config',
+	templateUrl: './promotion-config.component.html',
+	styleUrls: ['./promotion-config.component.scss'],
+	providers: [SortService, FilterService, DestroyService]
 })
 export class PromotionConfigComponent implements OnInit {
-  searchFormControl: FormControl;
-  dataSource: Array<IConfigPromotion>;
-  dataSourceTemp: Array<IConfigPromotion>;
+	searchFormControl: FormControl;
+	dataSource: Array<IConfigPromotion>;
+	dataSourceTemp: Array<IConfigPromotion>;
 
-  sorting: SortState;
+	sorting: SortState;
 
-  filterField: FilterField<{
-    nameProduct: null;
-    amountLiterOrder: null;
-    promotion: null
-  }>;
+	filterField: FilterField<{
+		nameProduct: null;
+		amountLiterOrder: null;
+		promotion: null;
+	}>;
 
-  constructor(
-    private configManagementService: ConfigurationManagementService,
-    private sortService: SortService<any>,
-    private filterService: FilterService<any>,
-    private cdr: ChangeDetectorRef,
-    private destroy$: DestroyService,
-    private modalService: NgbModal,
-    private toastr: ToastrService
-  ) {
-    this.dataSource = this.dataSourceTemp = [];
-    this.sorting = sortService.sorting;
-    this.filterField = new FilterField({
-      nameProduct: null,
-      amountLiterOrder: null,
-      promotion: null
-    });
-    this.searchFormControl = new FormControl();
-  }
+	constructor(
+		private configManagementService: ConfigurationManagementService,
+		private sortService: SortService<any>,
+		private filterService: FilterService<any>,
+		private cdr: ChangeDetectorRef,
+		private destroy$: DestroyService,
+		private modalService: NgbModal,
+		private toastr: ToastrService
+	) {
+		this.dataSource = this.dataSourceTemp = [];
+		this.sorting = sortService.sorting;
+		this.filterField = new FilterField({
+			nameProduct: null,
+			amountLiterOrder: null,
+			promotion: null
+		});
+		this.searchFormControl = new FormControl();
+	}
 
-  ngOnInit(): void {
-    this.getListConfigPromo();
+	ngOnInit(): void {
+		this.getListConfigPromo();
 
-    this.searchFormControl.valueChanges
-      .pipe(debounceTime(500), takeUntil(this.destroy$))
-      .subscribe((value) => {
-        if (value.trim()) {
-          this.filterField.setFilterFieldValue(value.trim());
-        } else {
-          this.filterField.setFilterFieldValue(null);
-        }
+		this.searchFormControl.valueChanges
+			.pipe(debounceTime(500), takeUntil(this.destroy$))
+			.subscribe((value) => {
+				if (value.trim()) {
+					this.filterField.setFilterFieldValue(value.trim());
+				} else {
+					this.filterField.setFilterFieldValue(null);
+				}
 
-        this.dataSource = this.sortService.sort(
-          this.filterService.filter(this.dataSourceTemp, this.filterField.field)
-        );
-        this.cdr.detectChanges();
-      });
-  }
+				this.dataSource = this.sortService.sort(
+					this.filterService.filter(this.dataSourceTemp, this.filterField.field)
+				);
+				this.cdr.detectChanges();
+			});
+	}
 
-  getListConfigPromo() {
-    this.configManagementService.getListConfigPromotion()
-      .subscribe(
-        (res) => {
-          this.dataSource = this.dataSourceTemp = res.data;
-          this.dataSource = this.sortService.sort(
-            this.filterService.filter(this.dataSourceTemp, this.filterField.field)
-          );
-          this.cdr.detectChanges();
-        }
-      )
-  }
+	getListConfigPromo() {
+		this.configManagementService.getListConfigPromotion().subscribe((res) => {
+			this.dataSource = this.dataSourceTemp = res.data;
+			this.dataSource = this.sortService.sort(
+				this.filterService.filter(this.dataSourceTemp, this.filterField.field)
+			);
+			this.cdr.detectChanges();
+		});
+	}
 
-  createModal($event?: Event, data?: IDataTransfer): void {
-    if ($event) {
-      $event.stopPropagation();
-    }
-    const modalRef = this.modalService.open(PromotionConfigModalComponent, {
-      backdrop: 'static',
-      size: 'xl'
-    });
+	createModal($event?: Event, data?: IDataTransfer): void {
+		if ($event) {
+			$event.stopPropagation();
+		}
+		const modalRef = this.modalService.open(PromotionConfigModalComponent, {
+			backdrop: 'static',
+			size: 'xl'
+		});
 
-    modalRef.componentInstance.data = {
-      title: data ? 'Sửa khuyến mại' : 'Thêm khuyến mại',
-      product: data
-    };
+		modalRef.componentInstance.data = {
+			title: data ? 'Sửa khuyến mại' : 'Thêm khuyến mại',
+			product: data
+		};
 
-    modalRef.result.then((result) => {
-      if (result) {
-        this.getListConfigPromo();
-      }
-    });
-  }
+		modalRef.result.then((result) => {
+			if (result) {
+				this.getListConfigPromo();
+			}
+		});
+	}
 
-  deleteConfig($event: Event, item: any) {
-    $event.stopPropagation();
-    const modalRef = this.modalService.open(ConfirmDeleteComponent, {
-      backdrop: 'static'
-    });
-    const data: IConfirmModalData = {
-      title: 'Xác nhận',
-      message: `Bạn có chắc chắn muốn xoá cấu hình khuyến mại?`,
-      button: { class: 'btn-primary', title: 'Xác nhận' }
-    };
-    modalRef.componentInstance.data = data;
+	deleteConfig($event: Event, item: any) {
+		$event.stopPropagation();
+		const modalRef = this.modalService.open(ConfirmDeleteComponent, {
+			backdrop: 'static'
+		});
+		const data: IConfirmModalData = {
+			title: 'Xác nhận',
+			message: `Bạn có chắc chắn muốn xoá cấu hình khuyến mại?`,
+			button: { class: 'btn-primary', title: 'Xác nhận' }
+		};
+		modalRef.componentInstance.data = data;
 
-    modalRef.result.then((result) => {
-      if (result) {
-        console.log(result);
-      }
-    });
-  }
+		modalRef.result.then((result) => {
+			if (result) {
+				console.log(result);
+			}
+		});
+	}
 
-  checkError(error: IError) {
-    console.log(error);
-  }
+	checkError(error: IError) {
+		console.log(error);
+	}
 
-  sort(column: string) {
-    this.dataSource = this.sortService.sort(this.dataSourceTemp, column);
-  }
+	sort(column: string) {
+		this.dataSource = this.sortService.sort(this.dataSourceTemp, column);
+	}
 }
