0b37ad0887f6031c74b631bbc5b3180336b9433f
QuangNV <quangnv1311@gmail.com>
2021-09-29 17:06:14 +0700

    hot fix

--- src/app/modules/contract/create-contract/create-contract.component.ts
+++ src/app/modules/contract/create-contract/create-contract.component.ts
@@ -741,7 +741,7 @@ export class CreateContractComponent implements OnInit, AfterViewInit {
 				fullAddress: contractData.fullAddress,
 				productInfoRequests: productData,
 				totalPayment: this.getTotal(),
-				attachmentRequests: this.filesUploaded.map((f) => f.id),
+				attachmentRequests: this.filesUploaded?.map((f) => f.id) || [],
 				statusType: status
 			};
 			if (!this.isUpdate) {
@@ -791,7 +791,7 @@ export class CreateContractComponent implements OnInit, AfterViewInit {
 					paymentTimeFive: convertDateToServer(contractData.payPlanDate5)
 				},
 				countPayment: this.payPlanDateCount,
-				attachmentRequests: this.filesUploaded.map((f) => f.id),
+				attachmentRequests: this.filesUploaded?.map((f) => f.id) || [],
 				statusType: status
 			};
 
