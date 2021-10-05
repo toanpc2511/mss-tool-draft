ad97d6cb4c2eb50cf0d1ee0b055f6089ed6f563b
ToanPC <toanpc@ttc-solutions.com.vn>
2021-10-05 17:35:11 +0700

    add ui scr qr code v2

--- src/app/modules/shift/shift-work-config-modal/shift-work-config-modal.component.ts
+++ src/app/modules/shift/shift-work-config-modal/shift-work-config-modal.component.ts
@@ -1,7 +1,10 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DestroyService } from '../../../shared/services/destroy.service';
+import { convertTimeToString, getHours, getMinutes, IHour, IMinute } from '../../../shared/helpers/functions';
+import { combineLatest, fromEvent, Observable, of } from 'rxjs';
+import { concatMap, debounceTime, startWith, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shift-work-config-modal',
@@ -13,32 +16,161 @@ export class ShiftWorkConfigModalComponent implements OnInit {
   @ViewChild('btnSave', { static: true }) btnSave: ElementRef;
   @Input() data: IDataTransfer;
 
-  hours: Array<any> = [];
-  minutes: Array<any> = [];
-  item;
-  item2;
+  shifts: Array<any> = [
+    {
+      id: 1,
+      name: 'Ca sáng'
+    },
+    {
+      id: 2,
+      name: 'Ca đêm'
+    },
+    {
+      id: 3,
+      name: 'Ca ngày'
+    },
+    {
+      id: 4,
+      name: 'Ca gãy'
+    },
+  ]
+  hours: Array<IHour> = [];
+  minutes: Array<IMinute> = [];
+  configForm: FormGroup;
+  timeBreakArray: FormArray;
+  timeStart: string;
+  timeEnd: string;
+  valueTimeBreak: string;
 
   constructor(
     public modal: NgbActiveModal,
+    private destroy$: DestroyService,
+    private cdr: ChangeDetectorRef,
     private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
-    for (let i = 0; i <= 24; i++) {
-      i < 10 ? this.item = (`0${i}`) : this.item = i.toString();
-      this.hours.push(this.item)
-    }
+    this.buildForm();
+    this.hours = getHours(48);
+    this.minutes = getMinutes();
 
-    for (let i = 0; i <= 60; i = i + 15) {
-      i < 10 ? this.item = (`0${i}`) : this.item = i.toString();
-      this.minutes.push(this.item)
-    }
+    this.combineShiftDetail();
+
+    this.onSubmit();
   }
 
-  add() {}
+  buildForm() {
+    this.configForm = this.fb.group({
+      nameShift: ['1', Validators.required],
+      hourStart: ['00'],
+      hourEnd: ['00'],
+      minutesStart: ['00'],
+      minutesEnd: ['00'],
+      timeBreak: this.fb.array([
+        this.fb.group({
+          hourStart: ['00'],
+          hourEnd: ['00'],
+          minutesStart: ['00'],
+          minutesEnd: ['00']
+        })
+      ]),
+      shiftDetail: ['']
+    })
+
+    this.timeBreakArray = this.configForm.get('timeBreak') as FormArray;
+    this.cdr.detectChanges();
+  }
+
+  combineShiftDetail() {
+    const nameShift$ = this.configForm
+      .get('nameShift')
+      .valueChanges.pipe(startWith(1), takeUntil(this.destroy$)) as Observable<number>;
+
+    const hourStart$ = this.configForm
+      .get('hourStart')
+      .valueChanges.pipe(startWith(''), takeUntil(this.destroy$)) as Observable<number>;
+
+    const minuteStart$ = this.configForm
+      .get('minutesStart')
+      .valueChanges.pipe(startWith(''), takeUntil(this.destroy$)) as Observable<number>;
+
+    const hourEnd$ = this.configForm
+      .get('hourEnd')
+      .valueChanges.pipe(startWith(''), takeUntil(this.destroy$)) as Observable<number>;
+
+    const minuteEnd$ = this.configForm
+      .get('minutesEnd')
+      .valueChanges.pipe(startWith(''), takeUntil(this.destroy$)) as Observable<number>;
+
+    const timeBreak$ = this.configForm
+      .get('timeBreak')
+      .valueChanges.pipe(startWith(''), takeUntil(this.destroy$)) as Observable<number>;
+
+    combineLatest([nameShift$, hourStart$, minuteStart$, hourEnd$, minuteEnd$, timeBreak$])
+      .pipe(
+        debounceTime(300),
+        concatMap(([nameShift, hourStart, minuteStart, hourEnd, minuteEnd]) =>
+          of({
+            nameShift,
+            hourStart,
+            minuteStart,
+            hourEnd,
+            minuteEnd
+          })
+        ),
+        tap((data) => {
+          const name: string = this.shifts.find((p) => p.id === Number(data.nameShift))?.name;
+          const hourStart: number = this.configForm.get('hourStart').value;
+          const minutesStart: number = this.configForm.get('minutesStart').value;
+          const hourEnd: number = this.configForm.get('hourEnd').value;
+          const minutesEnd: number = this.configForm.get('minutesEnd').value;
+          const valueTimeBreak: string = this.getListTimeBreak(this.configForm.get('timeBreak').value);
+
+          this.timeStart = convertTimeToString(hourStart, minutesStart);
+          this.timeEnd = convertTimeToString(hourEnd, minutesEnd);
+
+          const shiftDetail = `${name} ( ${this.timeStart} - ${this.timeEnd}), Nghỉ (${valueTimeBreak})`;
+
+          this.configForm.get('shiftDetail').patchValue(shiftDetail);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  getListTimeBreak(data: any) {
+    return data.map((x) => `${convertTimeToString(x.hourStart, x.minutesStart)} - ${convertTimeToString(x.hourEnd, x.minutesEnd)}`).join(', ');
+  }
+
+  addItem() {
+    this.timeBreakArray.push(
+      this.fb.group({
+        hourStart: ['00'],
+        hourEnd: ['00'],
+        minutesStart: ['00'],
+        minutesEnd: ['00']
+      })
+    );
+  }
+
+  deleteItem(index: number): void {
+    this.timeBreakArray.removeAt(index);
+  }
 
   onClose() {
     this.modal.close();
+  }
+
+  onSubmit(): void {
+    fromEvent(this.btnSave.nativeElement, 'click')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.configForm.markAllAsTouched();
+        if (this.configForm.invalid) {
+          return;
+        }
+        console.log(this.configForm.value);
+      });
   }
 }
 
