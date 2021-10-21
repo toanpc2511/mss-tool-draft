9b9a29b51a84f0b91ed9e235a44ebdfa63b7b861
ToanPC <toanpc@ttc-solutions.com.vn>
2021-10-21 17:13:11 +0700

    tích hợp api

--- src/app/modules/qr-code/qr-code.service.ts
+++ src/app/modules/qr-code/qr-code.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '../../shared/services/http.service';
+import { HttpParams } from '@angular/common/http';
+import { ISortData } from '../product/product.service';
+
+export interface IQrProductOther {
+  categoryId: number,
+  categoryName: string,
+  id: number,
+  price: number,
+  productName: string,
+  status: string,
+  valueAddedTax: number,
+  qrCodeProduct: {
+    id: number,
+    description: string,
+    title: string,
+    qrCodeImage: {
+      id: number,
+      typeMedia: string,
+      url: string
+    }
+  }
+}
+
+export interface IQrPumpHose {
+  code: string,
+  hose: string,
+  id: number,
+  nameFuel: string,
+  pole: null
+  price: number,
+  qrCode: null
+  station: string,
+  status: string,
+  image: {
+    id: number,
+    typeMedia: string,
+    url: string,
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class QrCodeService {
 
-  constructor() { }
+  constructor(
+    private http: HttpService
+  ) { }
+
+  getListQrCodeProductOther(page: number, size: number, sortData: ISortData) {
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString())
+      .set('field-sort', sortData?.fieldSort || '')
+      .set('direction-sort', sortData?.directionSort || '')
+    return this.http.get<Array<IQrProductOther>>('qrs/products/others', {params});
+  }
+
+  getListQrCodePumlHose() {
+    return this.http.get<Array<IQrPumpHose>>('qrs/hoses');
+  }
 }
