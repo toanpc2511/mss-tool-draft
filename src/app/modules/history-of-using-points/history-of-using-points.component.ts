c5dead6666e971bedb3ddc8eab267a7fba546a67
ToanPC <toanpc@ttc-solutions.com.vn>
2021-11-05 10:48:32 +0700

    done

--- src/app/modules/history-of-using-points/history-of-using-points.component.ts
+++ src/app/modules/history-of-using-points/history-of-using-points.component.ts
@@ -6,140 +6,179 @@ import { DestroyService } from '../../shared/services/destroy.service';
 import { FileService } from '../../shared/services/file.service';
 import * as moment from 'moment';
 import { IPaginatorState, PaginatorState } from '../../_metronic/shared/crud-table';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, tap } from 'rxjs/operators';
 import {
-  HistoryUsingPointsService,
-  IFilterUsingPoint,
-  IPaymentMethod,
-  IStationEployee
+	HistoryUsingPointsService,
+	IFilterUsingPoint,
+	IHistoryUsingPoint,
+	IPaymentMethod,
+	IStationEployee
 } from './history-of-using-points.service';
 import { convertDateToServer } from '../../shared/helpers/functions';
 import { IFilterTransaction } from '../transaction/transaction.service';
 
 @Component({
-  selector: 'app-history-of-using-points',
-  templateUrl: './history-of-using-points.component.html',
-  styleUrls: ['./history-of-using-points.component.scss'],
-  providers: [FormBuilder, DestroyService]
+	selector: 'app-history-of-using-points',
+	templateUrl: './history-of-using-points.component.html',
+	styleUrls: ['./history-of-using-points.component.scss'],
+	providers: [FormBuilder, DestroyService]
 })
 export class HistoryOfUsingPointsComponent implements OnInit {
-  dataSource = [];
-  searchForm: FormGroup;
-  paymentMethods: Array<IPaymentMethod> = [];
-  stationEmployee: Array<IStationEployee> = [];
-  dataProduct: Array<IProduct> = [];
-  firstDayOfMonth: string;
-  today: string;
-  paginatorState = new PaginatorState();
-  categoryId: number;
+	dataSource: IHistoryUsingPoint[] = [];
+	searchForm: FormGroup;
+	paymentMethods: Array<IPaymentMethod> = [];
+	stationEmployee: Array<IStationEployee> = [];
+	dataProduct: Array<IProduct> = [];
+	firstDayOfMonth: string;
+	today: string;
+	paginatorState = new PaginatorState();
+	categoryId: number;
 
-  totalRecord = 1;
+	totalAccumulationPointUse: number;
+	totalAccumulationPointReceive: number;
+	totalRecord: number;
 
-  constructor(
-    private fb: FormBuilder,
-    private modalService: NgbModal,
-    private cdr: ChangeDetectorRef,
-    private destroy$: DestroyService,
-    private fileService: FileService,
-    private productService: ProductService,
-    private historyUsingPointsService: HistoryUsingPointsService
-  ) {
-    this.firstDayOfMonth = moment().startOf('month').format('DD/MM/YYYY');
-    this.today = moment().format('DD/MM/YYYY');
-    this.init();
-  }
+	constructor(
+		private fb: FormBuilder,
+		private modalService: NgbModal,
+		private cdr: ChangeDetectorRef,
+		private destroy$: DestroyService,
+		private fileService: FileService,
+		private productService: ProductService,
+		private historyUsingPointsService: HistoryUsingPointsService
+	) {
+		this.firstDayOfMonth = moment().startOf('month').format('DD/MM/YYYY');
+		this.today = moment().format('DD/MM/YYYY');
+		this.init();
+	}
 
-  init() {
-    this.paginatorState.page = 1;
-    this.paginatorState.pageSize = 10;
-    this.paginatorState.pageSizes = [5, 10, 15, 20];
-    this.paginatorState.total = 0;
+	init() {
+		this.paginatorState.page = 1;
+		this.paginatorState.pageSize = 10;
+		this.paginatorState.pageSizes = [5, 10, 15, 20];
+		this.paginatorState.total = 0;
 
-    this.categoryId = 0;
+		this.categoryId = 0;
 
-    this.totalRecord = 0;
-  }
+		this.totalRecord = 0;
+		this.totalAccumulationPointUse = 0;
+		this.totalAccumulationPointReceive = 0;
+	}
 
-  ngOnInit(): void {
-    this.buildForm();
-    this.initDate();
-    this.getPaymentMethods();
-    this.getStationEmployee();
-    this.getListProductFuel();
-  }
+	ngOnInit(): void {
+		this.buildForm();
+		this.initDate();
+		this.getPaymentMethods();
+		this.getStationEmployee();
+		this.getListProductFuel();
 
-  getPaymentMethods() {
-    this.historyUsingPointsService
-      .getPaymentMethods()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((res) => {
-        this.paymentMethods = res.data;
-        this.cdr.detectChanges();
-      });
-  }
+		this.onSearch();
+	}
 
-  getStationEmployee() {
-    this.historyUsingPointsService
-      .getStationEmployee()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((res) => {
-        this.stationEmployee = res.data;
-        this.cdr.detectChanges();
-      });
-  }
+	getPaymentMethods() {
+		this.historyUsingPointsService
+			.getPaymentMethods()
+			.pipe(takeUntil(this.destroy$))
+			.subscribe((res) => {
+				this.paymentMethods = res.data;
+				this.cdr.detectChanges();
+			});
+	}
 
-  getListProductFuel() {
-    this.productService
-      .getListProduct(this.categoryId)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((res) => {
-        this.dataProduct = res.data;
-        this.cdr.detectChanges();
-      });
-  }
+	getStationEmployee() {
+		this.historyUsingPointsService
+			.getStationEmployee()
+			.pipe(takeUntil(this.destroy$))
+			.subscribe((res) => {
+				this.stationEmployee = res.data;
+				this.cdr.detectChanges();
+			});
+	}
 
-  initDate() {
-    this.searchForm.get('startAt').patchValue(this.firstDayOfMonth);
-    this.searchForm.get('endAt').patchValue(this.today);
-  }
+	getListProductFuel() {
+		this.productService
+			.getListProduct(this.categoryId)
+			.pipe(takeUntil(this.destroy$))
+			.subscribe((res) => {
+				this.dataProduct = res.data;
+				this.cdr.detectChanges();
+			});
+	}
 
-  buildForm() {
-    this.searchForm = this.fb.group({
-      orderCode: [''],
-      phone: [''],
-      userName: [''],
-      startAt: [''],
-      endAt: [''],
-      paymentMethod: [''],
-      stationName: [''],
-      productName: ['']
-    })
-  }
+	initDate() {
+		this.searchForm.get('startAt').patchValue(this.firstDayOfMonth);
+		this.searchForm.get('endAt').patchValue(this.today);
+	}
 
-  pagingChange($event: IPaginatorState) {
-    this.paginatorState = $event as PaginatorState;
-    this.onSearch();
-  }
+	buildForm() {
+		this.searchForm = this.fb.group({
+			orderCode: [''],
+			phone: [''],
+			userName: [''],
+			startAt: [''],
+			endAt: [''],
+			paymentMethod: [''],
+			stationName: [''],
+			productName: ['']
+		});
+	}
 
-  onSearch() {
-    console.log(this.searchForm.value);
+	pagingChange($event: IPaginatorState) {
+		this.paginatorState = $event as PaginatorState;
+		this.onSearch();
+	}
 
-    const filterData: IFilterUsingPoint = this.getFilterData();
-  }
+	onSearch() {
+		const filterData: IFilterUsingPoint = this.getFilterData();
 
-  getFilterData() {
-    const filterFormData: IFilterUsingPoint = this.searchForm.value;
-    return {
-      ...filterFormData,
-      startAt: convertDateToServer(filterFormData.startAt),
-      endAt: convertDateToServer(filterFormData.endAt)
-    };
-  }
+		this.historyUsingPointsService
+			.searchHistoryUsingPoints(this.paginatorState.page, this.paginatorState.pageSize, filterData)
+			.subscribe((res) => {
+				this.dataSource = res.data;
+				if (res.data) {
+					this.dataSource = res.data;
+					if (this.dataSource.length > 0) {
+						this.totalRecord = this.dataSource[0].total;
+						this.totalAccumulationPointUse = this.dataSource[0].totalAccumulationPointUse;
+						this.totalAccumulationPointReceive = this.dataSource[0].totalAccumulationPointReceive;
+					} else {
+						this.totalRecord = 0;
+						this.totalAccumulationPointUse = 0;
+						this.totalAccumulationPointReceive = 0;
+					}
 
-  onReset() {
-    this.ngOnInit();
-  }
+					this.totalRecord = res.meta.total;
+					this.paginatorState.recalculatePaginator(res.meta.total);
+					this.cdr.detectChanges();
+				}
+			});
+	}
 
-  exportFileExcel() {}
+	getFilterData() {
+		const filterFormData: IFilterUsingPoint = this.searchForm.value;
+		return {
+			...filterFormData,
+			startAt: convertDateToServer(filterFormData.startAt),
+			endAt: convertDateToServer(filterFormData.endAt)
+		};
+	}
 
+	exportFileExcel() {
+		const filterData: IFilterUsingPoint = this.getFilterData();
+		this.historyUsingPointsService
+			.exportFileExcel(filterData)
+			.pipe(
+				tap((res) => {
+					if (res) {
+						this.fileService.downloadFromUrl(res.data);
+					}
+				}),
+				takeUntil(this.destroy$)
+			)
+			.subscribe();
+	}
+
+	onReset() {
+		this.ngOnInit();
+	}
 }
