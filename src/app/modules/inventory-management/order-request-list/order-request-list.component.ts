a899578e6a41d3574c678a70ea516ee8d8e09aee
ToanPC <toanpc@ttc-solutions.com.vn>
2021-11-09 17:35:30 +0700

    tích hợp api tìm kiếm yêu cầu đặt hàng

--- src/app/modules/inventory-management/order-request-list/order-request-list.component.ts
+++ src/app/modules/inventory-management/order-request-list/order-request-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { InventoryManagementService } from '../inventory-management.service';
+import { IEmployees, IFilterTransaction, InventoryManagementService } from '../inventory-management.service';
 import * as moment from 'moment';
 import { IPaginatorState, PaginatorState } from '../../../_metronic/shared/crud-table';
 import { Router } from '@angular/router';
+import { IStationEployee } from '../../history-of-using-points/history-of-using-points.service';
+import { concatMap, takeUntil, tap } from 'rxjs/operators';
+import { DestroyService } from '../../../shared/services/destroy.service';
+import { NO_EMIT_EVENT } from '../../../shared/app-constants';
+import { of } from 'rxjs';
+import { convertDateToServer } from '../../../shared/helpers/functions';
+import { IError } from '../../../shared/models/error.model';
+import { ToastrService } from 'ngx-toastr';
+import { LIST_STATUS_ORDER_REQUEST } from '../../../shared/data-enum/list-status';
 
 @Component({
   selector: 'app-order-request-list',
   templateUrl: './order-request-list.component.html',
   styleUrls: ['./order-request-list.component.scss'],
-  providers: [FormBuilder]
+  providers: [FormBuilder, DestroyService]
 })
 export class OrderRequestListComponent implements OnInit {
   today: string;
@@ -17,11 +26,17 @@ export class OrderRequestListComponent implements OnInit {
   paginatorState = new PaginatorState();
   searchForm: FormGroup;
   dataSource;
+  stationEmployee: Array<IStationEployee> = [];
+  listEmployees: Array<IEmployees> = [];
+  listStatus = LIST_STATUS_ORDER_REQUEST;
 
   constructor(
     private fb: FormBuilder,
     private inventoryManagementService: InventoryManagementService,
     private router: Router,
+    private destroy$: DestroyService,
+    private cdr: ChangeDetectorRef,
+    private toastr: ToastrService,
   ) {
     this.firstDayOfMonth = moment().startOf('month').format('DD/MM/YYYY');
     this.today = moment().format('DD/MM/YYYY');
@@ -39,28 +54,111 @@ export class OrderRequestListComponent implements OnInit {
   ngOnInit(): void {
     this.buildForm();
     this.initDate();
+
+    this.getStationEmployee();
+    this.getAllEmployee();
+    this.onSearch();
+
+    this.handleStationChange();
   }
 
   buildForm() {
     this.searchForm = this.fb.group({
-      stationName: [''],
-      nguoiYeuCau: [''],
-      startAt: [],
-      endAt: [],
+      stationId: [''],
+      employeeId: [''],
+      expectedDate: [],
+      approvalDate: [],
       status: ['']
     })
   }
 
   initDate() {
-    this.searchForm.get('startAt').patchValue(this.firstDayOfMonth);
-    this.searchForm.get('endAt').patchValue(this.today);
+    this.searchForm.get('expectedDate').patchValue(this.firstDayOfMonth);
+    this.searchForm.get('approvalDate').patchValue(this.today);
+  }
+
+  getStationEmployee() {
+    this.inventoryManagementService
+      .getStationEmployee()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res) => {
+        this.stationEmployee = res.data;
+        this.cdr.detectChanges();
+      });
+  }
+
+  getAllEmployee() {
+    this.inventoryManagementService
+      .getAllEmployee()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res) => {
+        this.listEmployees = res.data;
+        this.cdr.detectChanges();
+      });
+  }
+
+  handleStationChange() {
+    this.searchForm
+      .get('stationId')
+      .valueChanges.pipe(
+      concatMap((stationName: string) => {
+        this.listEmployees = [];
+        this.searchForm.get('employeeId').reset('', NO_EMIT_EVENT);
+        if (stationName) {
+          return this.inventoryManagementService.getEmployeeStation(stationName);
+        } else {
+          return this.inventoryManagementService.getAllEmployee();
+        }
+        return of(null);
+      }),
+      tap((res) => {
+        this.listEmployees = res.data;
+        this.cdr.detectChanges();
+      }),
+      takeUntil(this.destroy$)
+    )
+      .subscribe();
   }
 
   createOrder() {
     this.router.navigate(['/kho/yeu-cau-dat-hang/them-moi']);
   }
 
-  onSearch() {}
+  getFilterData() {
+    const filterFormData: IFilterTransaction = this.searchForm.value;
+    return {
+      ...filterFormData,
+      expectedDate: convertDateToServer(filterFormData.expectedDate),
+      approvalDate: convertDateToServer(filterFormData.approvalDate)
+    };
+  }
+
+  onSearch() {
+    const filterData: IFilterTransaction = this.getFilterData();
+    console.log(filterData);
+
+    this.inventoryManagementService
+      .searchOrderRequest(this.paginatorState.page, this.paginatorState.pageSize, filterData)
+      .subscribe((res) => {
+        if (res.data) {
+          this.dataSource = res.data;
+
+          this.paginatorState.recalculatePaginator(res.meta.total);
+          this.cdr.detectChanges();
+        }
+      },
+        (err: IError) => {
+          this.checkError(err);
+        });
+  }
+
+  updateOrderRequest($event: Event, id: number) {
+    console.log(id);
+  }
+
+  deleteOrderRequest($event: Event, item) {
+    console.log(item);
+  }
 
   onReset() {
     this.ngOnInit();
@@ -69,6 +167,10 @@ export class OrderRequestListComponent implements OnInit {
   pagingChange($event: IPaginatorState) {
     this.paginatorState = $event as PaginatorState;
     this.onSearch();
+  }
+
+  checkError(error: IError) {
+    this.toastr.error(error.code);
   }
 
 }
