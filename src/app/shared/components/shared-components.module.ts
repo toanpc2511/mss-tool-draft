1a1fb7678548e07e6474049cac6396375a8f1c14
Nguyễn Văn Quang <quangnv@ttc-solutions.com.vn>
2021-12-02 09:02:39 +0700

    update

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -1,3 +1,4 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -6,16 +7,18 @@ import { ConfirmDeleteComponent } from './confirm-delete/confirm-delete.componen
 import { NgSelectModule } from './ng-select/public-api';
 import { TabComponent } from './tab/tab.component';
 import { ListLayoutComponent } from './list-layout/list-layout.component';
+import { TwoFactorComponent } from './two-factor/two-factor.component';
 
 @NgModule({
-	declarations: [ValidateComponent, ConfirmDeleteComponent, TabComponent, ListLayoutComponent],
-	imports: [CommonModule, NgSelectModule, RouterModule],
+	declarations: [ValidateComponent, ConfirmDeleteComponent, TabComponent, ListLayoutComponent, TwoFactorComponent],
+	imports: [CommonModule, NgSelectModule, RouterModule, ReactiveFormsModule],
 	exports: [
 		ValidateComponent,
 		NgSelectModule,
 		ConfirmDeleteComponent,
 		TabComponent,
-		ListLayoutComponent
+		ListLayoutComponent,
+		TwoFactorComponent
 	]
 })
 export class SharedComponentsModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,15 @@ export const environment = {
 	USERDATA_KEY: 'authf649fc9a5f55',
 	isMockEnabled: false,
 	apiUrl: 'https://sunoil-management.firecloud.live/management',
-	apiUrlRoot: 'https://sunoil-management.firecloud.live'
+	apiUrlRoot: 'https://sunoil-management.firecloud.live',
+	firebase: {
+		apiKey: 'AIzaSyBwXgkaZIpdgdt9pfF5D2A3wOSstLGEBE8',
+		authDomain: 'sun-oil-admin.firebaseapp.com',
+		projectId: 'sun-oil-admin',
+		storageBucket: 'sun-oil-admin.appspot.com',
+		messagingSenderId: '330576029994',
+		appId: '1:330576029994:web:8727d7da0207dea2bd5653'
+	}
 };
 
 /*
