711043e24f23ced04010c83454dc307e8b367b41
ToanPC <toanpc@ttc-solutions.com.vn>
2021-12-14 09:10:16 +0700

    hot fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,22 +2,37 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+// 	production: false,
+// 	appVersion: '1.0.0',
+// 	USERDATA_KEY: 'authf649fc9a5f55',
+// 	isMockEnabled: false,
+// 	apiUrl: 'https://sunoil-management.firecloud.live/management',
+// 	apiUrlRoot: 'https://sunoil-management.firecloud.live',
+// 	firebase: {
+//     apiKey: "AIzaSyBGDWDvuG1Wq6Wbu9fBhaU86WdD2B7Q6Mo",
+//     authDomain: "sun-oil-e747c.firebaseapp.com",
+//     projectId: "sun-oil-e747c",
+//     storageBucket: "sun-oil-e747c.appspot.com",
+//     messagingSenderId: "621967318770",
+//     appId: "1:621967318770:web:f0b639097f7ae10b9a7a6b",
+//     measurementId: "G-DB7RBLVY70"
+// 	}
+// };
 export const environment = {
-	production: false,
-	appVersion: '1.0.0',
-	USERDATA_KEY: 'authf649fc9a5f55',
-	isMockEnabled: false,
-	apiUrl: 'https://sunoil-management.firecloud.live/management',
-	apiUrlRoot: 'https://sunoil-management.firecloud.live',
-	firebase: {
-        apiKey: "AIzaSyCk4MnYZHLArwwzF6T7Zahn-v2u0z1Qn48",
-        authDomain: "fir-demo-71b62.firebaseapp.com",
-        projectId: "fir-demo-71b62",
-        storageBucket: "fir-demo-71b62.appspot.com",
-        messagingSenderId: "40131388830",
-        appId: "1:40131388830:web:37993fdb9e3097f02c228f",
-        measurementId: "G-Y3WXWVG4BL"
-	}
+  production: false,
+  appVersion: '1.0.0',
+  apiUrl: 'https://sunoil-management.firecloud.live/management',
+  apiUrlRoot: 'https://sunoil-management.firecloud.live',
+  firebase: {
+    apiKey: "AIzaSyBGDWDvuG1Wq6Wbu9fBhaU86WdD2B7Q6Mo",
+    authDomain: "sun-oil-e747c.firebaseapp.com",
+    projectId: "sun-oil-e747c",
+    storageBucket: "sun-oil-e747c.appspot.com",
+    messagingSenderId: "621967318770",
+    appId: "1:621967318770:web:f0b639097f7ae10b9a7a6b",
+    measurementId: "G-DB7RBLVY70"
+  }
 };
 
 /*
