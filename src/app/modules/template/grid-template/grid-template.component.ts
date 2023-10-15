8f36c46b4dc82904a1041ae8cff46affcea0da80
ToanPC <phamcongtoan25111998@gmail.com>
2023-10-15 19:39:46 +0700

    update template v2

--- src/app/modules/template/grid-template/grid-template.component.ts
+++ src/app/modules/template/grid-template/grid-template.component.ts
@@ -1,6 +1,4 @@
 import { ChangeDetectorRef, Component, OnInit, Output } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
 import { EventEmitter } from '@angular/core';
 
 @Component({
@@ -23,20 +21,7 @@ export class GridTemplateComponent implements OnInit {
   elementsSelected: IElement[] = [];
   @Output() elementSelected: EventEmitter<any[]> = new EventEmitter();
 
-  constructor(
-    iconRegistry: MatIconRegistry,
-    sanitizer: DomSanitizer,
-    private cdr: ChangeDetectorRef
-  ) {
-    iconRegistry.addSvgIcon(
-      'apple',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/apple-icon.svg')
-    );
-    iconRegistry.addSvgIcon(
-      'windows',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/windows-icon.svg')
-    );
-  }
+  constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.generateLayout();
@@ -46,10 +31,13 @@ export class GridTemplateComponent implements OnInit {
   generateLayout(): void {
     const numberCol = 12;
     const numberRow = 10;
+    let index = 1;
     for (let y = 1; y <= numberRow; y++) {
       for (let x = 1; x <= numberCol; x++) {
         this.elements.push({
-          id: `${x}-${y}`,
+          id: index++,
+          x: x,
+          y: y,
           name: `${x}-${y}`,
           disabled: false,
           cols: 1,
@@ -62,33 +50,26 @@ export class GridTemplateComponent implements OnInit {
 
   //
   onLoadLayout(): void {
-    const elFirst = this.handleCoordinates(this.elementsSelected[0]);
-    const elLast = this.handleCoordinates(this.elementsSelected.slice(-1)[0]);
-    const el = {
-      id: this.elementsSelected[0].id,
-      name: `${this.elementsSelected[0].name}/${
-        this.elementsSelected.slice(-1)[0].name
-      }`,
-      disabled: true,
+    const elFirst = this.elementsSelected[0];
+    const elLast = this.elementsSelected.slice(-1)[0];
+
+    const elNew: IElement = {
+      id: elFirst.id,
+      name: `${elFirst.name}/${elLast.name}`,
+      x: elFirst.x,
+      y: elFirst.y,
       cols: elLast.x - elFirst.x + 1,
       rows: elLast.y - elFirst.y + 1,
+      disabled: true,
       img: 'assets/bg.jpeg',
     };
-    this.elements.splice(
-      this.elements.indexOf(this.elementsSelected[0]),
-      0,
-      el
-    );
+    this.elements.splice(this.elements.indexOf(elFirst), 0, elNew);
     this.elements = this.elements.filter(
       (el) => !this.elementsSelected.includes(el)
     );
-    // console.log(this.elements);
-
-    this.cdr.detectChanges();
   }
 
   deleteLayout(value: IElement): void {
-    console.log(value);
     const indexKey = value.name.indexOf('/');
     const elFirst = this.handleCoordinates(value.name.slice(0, indexKey));
     const elLast = this.handleCoordinates(value.name.slice(indexKey + 1));
@@ -97,8 +78,10 @@ export class GridTemplateComponent implements OnInit {
     for (let y = elFirst.y; y <= elLast.y; y++) {
       for (let x = elFirst.x; x <= elLast.x; x++) {
         newArr.push({
-          id: `${x}-${y}`,
+          id: (y - 1) * 12 + x,
           name: `${x}-${y}`,
+          x: x,
+          y: y,
           disabled: false,
           cols: 1,
           rows: 1,
@@ -106,11 +89,7 @@ export class GridTemplateComponent implements OnInit {
         });
       }
     }
-    // this.elements = this.elements.concat(newArr)
     this.elements = this.sortData(this.elements.concat(newArr));
-
-    // this.elements = this.elements.filter((el) => el.name !== value.name);
-    this.cdr.detectChanges();
   }
 
   // event selected
@@ -119,24 +98,12 @@ export class GridTemplateComponent implements OnInit {
   }
 
   sortData(data: IElement[]): IElement[] {
-    const result = data.sort(
-      (a, b) =>
-        this.handleCoordinates(a).x - this.handleCoordinates(b).x ||
-        this.handleCoordinates(a).y - this.handleCoordinates(b).y
-    );
-    return result;
+    return data.sort((a, b) => a.id - b.id);
   }
 
   // Xử lý toạ độ element
-  handleCoordinates(data: IElement | string): { x: number; y: number } {
-    const indexKey =
-      typeof data === 'string' ? data.indexOf('-') : data.id.indexOf('-');
-    if (typeof data !== 'string') {
-      return {
-        x: Number(data.id.slice(0, indexKey)),
-        y: Number(data.id.slice(indexKey + 1)),
-      };
-    }
+  handleCoordinates(data: string): { x: number; y: number } {
+    const indexKey = data.indexOf('-');
     return {
       x: Number(data.slice(0, indexKey)),
       y: Number(data.slice(indexKey + 1)),
@@ -145,10 +112,12 @@ export class GridTemplateComponent implements OnInit {
 }
 
 export interface IElement {
-  id: string;
+  id: number;
   name: string;
   cols: number;
   rows: number;
   img: string;
   disabled: boolean;
+  x: number;
+  y: number;
 }
